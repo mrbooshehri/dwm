686a27ff8d8d982d3818de108655e9c13db37f82
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-04-14 15:51:10 +0300

    view all tags added

--- config.def.h
+++ config.def.h
@@ -294,6 +294,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,      scratchpad_show,        {0} },
 	{ MODKEY|ShiftMask,             XK_minus,      scratchpad_hide,        {0} },
 	{ MODKEY,                       XK_equal,      scratchpad_remove,      {0} },
+	{ MODKEY,                       XK_0,	       view,                   {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,          tag,                    {.ui = ~0 } },
 	TAGKEYS(                        XK_1,                                  0)
 	TAGKEYS(                        XK_2,                                  1)
 	TAGKEYS(                        XK_3,                                  2)
