c893450374ba2e7b871458231effd6749987a1b3
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-04-14 15:21:45 +0300

    minor edits

--- config.def.h
+++ config.def.h
@@ -283,7 +283,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_d,          incnmaster,             {.i = -1 } },
 	{ MODKEY,                       XK_h,          setmfact,               {.f = -0.05} },
 	{ MODKEY,                       XK_l,          setmfact,               {.f = +0.05} },
-	{ MODKEY,                       XK_Return,     zoom,                   {0} },
+	{ MODKEY|ControlMask,           XK_Return,     zoom,                   {0} },
 	{ MODKEY,                       XK_Tab,        view,                   {0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_q,          quit,                   {0} },
 	{ MODKEY|ShiftMask,		XK_q,          killclient,             {0} },
